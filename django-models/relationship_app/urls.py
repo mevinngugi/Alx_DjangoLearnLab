a58fd9c6d5f0f006a881fe275d7020c6f80cd845
Mevin Ngugi <mevinngugi@ONAF-KE-MNGUGI.local>
2024-08-24 11:53:11 +0300

    Set up login, logout, and register users. Added style.css

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import list_books
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.base import TemplateView
 
-app_name = "relationship_app"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", TemplateView.as_view(template_name="relationship_app/home.html"), name="home"),
     path("list_books/", views.list_books, name="list_books"),
     #path("<int:book_id>/", views.detail, name="detail"),
     path("about/", views.AboutView.as_view(), name="about"),
-    path("<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail")
+    path("<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="register"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 #from django.template import loader
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 from .models import Book
 from .models import Library
@@ -32,7 +34,7 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     #library = Library.objects.get(name="Makadara")
-    #     #books_in_library = library.books.all()
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "relationship_app/register.html"
