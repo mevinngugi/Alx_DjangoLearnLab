16e04bf38c099190d5271541160b0596a3925de9
Mevin Ngugi <mevinngugi@ONAF-KE-MNGUGI.local>
2024-08-25 22:11:50 +0300

    Update permissions on view.py to reflect those that were created

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,14 +5,18 @@ from .models import Book
 # Create your views here.
 
 # Did not create the html files 
-@permission_required("bookshelf.can_add_book", raise_exception=True)
+@permission_required("bookshelf.can_create", raise_exception=True)
 def add_book(request):
     return render(request, "bookshelf/add_book.html")
 
-@permission_required("bookshelf.can_change_book", raise_exception=True)
+@permission_required("bookshelf.can_view", raise_exception=True)
+def list_book(request):
+    return render(request, "bookshelf/list_book.html")
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
 def edit_book(request):
     return render(request, "bookshelf/edit_book.html")
 
-@permission_required("bookshelf.can_delete_book", raise_exception=True)
+@permission_required("bookshelf.can_delete", raise_exception=True)
 def delete_book(request):
     return render(request, "bookshelf/delete_book.html")
