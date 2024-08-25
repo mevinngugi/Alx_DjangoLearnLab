2a864c29b2e45721428c8c7b2136ee3a105c1dcd
Mevin Ngugi <mevinngugi@ONAF-KE-MNGUGI.local>
2024-08-25 22:46:07 +0300

    Add url to the register view

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("add_book/", views.add_book, name="add_book"),
     path("edit_book/", views.edit_book, name="edit_book"),
     path("delete_book/", views.delete_book, name="delete_book"),
+    path("register/", views.register, name="register"),
 ]
