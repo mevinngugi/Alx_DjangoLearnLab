ca4a9c48384f39b357d1795e35544b6af47ce226
Mevin Ngugi <mevinngugi@ONAF-KE-MNGUGI.local>
2024-08-25 20:47:36 +0300

    revert or comment out customuser changes on relationship_app

--- advanced_features_and_security/relationship_app/admin.py
+++ advanced_features_and_security/relationship_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import UserProfile, CustomUser
+from .models import UserProfile
+#from .models import CustomUser
 
 # Register your models here.
 class UserProfileAdmin(admin.ModelAdmin):
@@ -8,22 +9,22 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 admin.site.register(UserProfile, UserProfileAdmin)
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    # fieldsets is used to define how fields are grouped when you view or edit a user in the admin panel.
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {"fields": ("date_of_birth", "profile_photo")}),
-    )
+# class CustomUserAdmin(UserAdmin):
+#     model = CustomUser
+#     # fieldsets is used to define how fields are grouped when you view or edit a user in the admin panel.
+#     fieldsets = UserAdmin.fieldsets + (
+#         (None, {"fields": ("date_of_birth", "profile_photo")}),
+#     )
 
-    # add_fieldsets is used to define how fields are grouped on the user creation form in the admin panel.
-    add_fieldsets = UserAdmin.fieldsets + (
-        (None, {"fields": ("date_of_birth", "profile_photo")}),
-    )
-    list_display = ["username",
-                    "email",
-                    "first_name",
-                    "last_name",
-                    "date_of_birth",
-                    "profile_photo"]
+#     # add_fieldsets is used to define how fields are grouped on the user creation form in the admin panel.
+#     add_fieldsets = UserAdmin.fieldsets + (
+#         (None, {"fields": ("date_of_birth", "profile_photo")}),
+#     )
+#     list_display = ["username",
+#                     "email",
+#                     "first_name",
+#                     "last_name",
+#                     "date_of_birth",
+#                     "profile_photo"]
 
-admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -62,32 +62,32 @@ def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
 
 # Week 11 - CustomUser Model
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, date_of_birth=None, profile_photo=None, password=None, **kwargs):
-        if not email:
-            raise ValueError("The Email field is required")
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, date_of_birth=date_of_birth, profile_photo=profile_photo, **kwargs)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, username, date_of_birth=None, profile_photo=None, password=None, **kwargs):
+#         if not email:
+#             raise ValueError("The Email field is required")
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, username=username, date_of_birth=date_of_birth, profile_photo=profile_photo, **kwargs)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
 
-    def create_superuser(self, email, username, password=None, **kwargs):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
+#     def create_superuser(self, email, username, password=None, **kwargs):
+#         extra_fields.setdefault("is_staff", True)
+#         extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+#         if extra_fields.get("is_staff") is not True:
+#             raise ValueError("Superuser must have is_staff=True.")
+#         if extra_fields.get("is_superuser") is not True:
+#             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self.create_user(email, username, password=password, **kwargs)
+#         return self.create_user(email, username, password=password, **kwargs)
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="uploads/images", null=True, blank=True)
+# class CustomUser(AbstractUser):
+#     date_of_birth = models.DateField(null=True, blank=True)
+#     profile_photo = models.ImageField(upload_to="uploads/images", null=True, blank=True)
 
-    objects = CustomUserManager()
+#     objects = CustomUserManager()
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
