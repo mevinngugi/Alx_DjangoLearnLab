22a2c288ae8c71e8f5ab58bfd372d3cbf386bbfd
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-09-15 22:09:12 +0300

    Set Up User Authentication Views, Create Templates for Authentication, Configure URL Patterns

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    # Login, Logout views
+    path('', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name="logout"),
+
+    # Redirects to home after login
+    path('home/', TemplateView.as_view(template_name='blog/home.html'), name='home'),
+
+    #Nav bar redirects
+    path('posts/', TemplateView.as_view(template_name='blog/posts.html'), name='posts'),
+    path('register/', TemplateView.as_view(template_name='blog/register.html'), name='register'),
+
+]
