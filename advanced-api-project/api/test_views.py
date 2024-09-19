f6ad5d34bcbde9a35509c6cf81b6feef56585f74
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-09-19 12:40:51 +0300

    Import and use status instead of hard codding

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -3,6 +3,7 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 from .models import Author, Book
 from django.contrib.auth.models import User, Permission
+from rest_framework import status
 #import json
 
 class BookAPITestCase(TestCase):
@@ -25,15 +26,15 @@ class BookAPITestCase(TestCase):
 
     def test_book_list(self):
         response = self.client.get(reverse('books'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_book_detail(self):
         response = self.client.get(reverse('book_detail', kwargs={'pk': self.test_book.pk}))
         #print("Response Content:", response.content)
         #response_data = response.json()
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         #print("Response Content as json:", response_data)
-        self.assertEqual(response_data['title'], self.test_book.title)
+        self.assertEqual(response.data['title'], self.test_book.title)
 
     def test_book_update_user_not_logged_in(self):
         response = self.client.put(
@@ -45,7 +46,7 @@ class BookAPITestCase(TestCase):
 
         #response_data = response.json()
         #print("Response Content as json:", response_data)
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_book_update_user_logged_in_WITHOUT_perms(self):
         self.client.login(username = self.test_user.username, password = 'testpassword')
@@ -60,7 +61,7 @@ class BookAPITestCase(TestCase):
         #response_data = response.json()
         #print("Response Content:", response.content)
         #print("Response Content as json:", response_data)
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_book_update_user_logged_in_with_perms(self):
         self.client.login(username = self.test_user.username, password = 'testpassword')
@@ -80,7 +81,7 @@ class BookAPITestCase(TestCase):
         #response_data = response.json()
         #print("Response Content:", response.content)
         #print("Response Content as json:", response_data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Test Book Updated Title')
         self.test_user.user_permissions.remove(permissions)
         #print("Response.data: ", response.data)
@@ -97,7 +98,7 @@ class BookAPITestCase(TestCase):
             'author': self.test_author.id,
             'publication_year': '2023-05-01 00:00:00'            
         })
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         #response_data = response.json()
         #print("Response Content:", response.content)
         #print("Response Content as json:", response_data)
@@ -110,7 +111,7 @@ class BookAPITestCase(TestCase):
 
         response = self.client.delete(
             reverse('delete_book', kwargs={'pk': self.test_book.pk}))
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_book_user_logged_in_with_perms(self):
         self.client.login(username=self.test_user.username, password='testpassword')
@@ -120,4 +121,4 @@ class BookAPITestCase(TestCase):
 
         response = self.client.delete(
             reverse('delete_book', kwargs={'pk': self.test_book.pk}))
-        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
