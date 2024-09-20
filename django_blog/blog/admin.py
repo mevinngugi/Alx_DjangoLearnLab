27ea591747ba1658becae1b2f83e013c44e494b2
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-09-20 11:00:04 +0300

    Registered Comment model to admin site

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Profile, Post
+from .models import Profile, Post, Comment
 
 # Register your models here.
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'bio', 'profile_picture']
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content', 'published_date', 'author']
+    list_display = ['id', 'user', 'bio', 'profile_picture']
 
 admin.site.register(Profile, ProfileAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'published_date', 'author']
+
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'post', 'author', 'created_at', 'updated_at']
+
+admin.site.register(Comment, CommentAdmin)
