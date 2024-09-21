d55f5376e69c5350081eb304d0d07389bc6c83f5
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2024-09-21 09:41:10 +0300

    Add widgets to post create form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
 from .models import Profile, Post, Comment
 from django import forms
+from taggit.forms import TagWidget
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -29,6 +30,11 @@ class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        # Adding widgets to handle the tag input. 
+        # Seems redundant as tag input was already handled by adding tags to the fields
+        widgets = {
+            'tags': TagWidget(), 
+        }
 
 # I could have probably reused the PostCreateForm
 # But in the case where you don't want to update the same fields as create form
